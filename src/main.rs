dce1ee77928f79344661491170d0c982a3823d13
nilq <i.am@nilq.dk>
2017-05-21 13:02:51 +0200

    Add lexer and .gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+extern crate rustyline;
+
+use rustyline::completion::FilenameCompleter;
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+mod sabri;
+use sabri::syntax;
+
+use syntax::lexer;
+
+use lexer::{BlockTree, process_branch};
+
+static PROMPT: &'static str = ">> ";
+
+#[allow(dead_code)]
+fn repl() {
+    let mut rl = rustyline::Editor::<()>::new();
+
+    loop {
+        let readline = rl.readline(PROMPT);
+        match readline {
+            Ok(line) => {
+                let mut blocks = BlockTree::new(line.as_str(), 0);
+                let indents    = blocks.indents();
+
+                let root = blocks.tree(&indents);
+                let done = process_branch(&root);
+
+                println!("{:#?}", done)
+            }
+
+            Err(ReadlineError::Interrupted) => {
+                println!("interrupted");
+                break
+            }
+
+            Err(ReadlineError::Eof) => {
+                println!("eof");
+                break
+            }
+
+            Err(err) => {
+                println!("error: {:?}", err);
+                break
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn test() {
+    let test = r#"
+~ a comment
+greet := |a|
+  print("yo, " + a)
+
+hello_world = | greet("world")
+"#;
+
+    let mut blocks = BlockTree::new(test, 0);
+    let indents    = blocks.indents();
+
+    let root = blocks.tree(&indents);
+    let done = process_branch(&root);
+
+    println!("{:#?}", done)
+}
+
 fn main() {
-    println!("Hello, world!");
+    repl()
 }
--- src/sabri/syntax/mod.rs
+++ src/sabri/syntax/mod.rs
@@ -1,13 +1,1 @@
-pub mod token;
-pub mod block_tree;
-pub mod tokenizer;
-pub mod matcher;
-
-mod lexer;
-
-pub use self::token::{Token, TokenType, TokenPosition};
-pub use self::matcher::Matcher;
-pub use self::tokenizer::Tokenizer;
-pub use self::block_tree::{BlockTree, Chunk, ChunkValue};
-
-pub use self::lexer::{lexer, lex_branch, flatten_branch, process_branch};
+pub mod lexer;
