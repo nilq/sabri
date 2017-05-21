40cbc6ce3078f88ae51dd6eb98b4b4324d07013a
nilq <i.am@nilq.dk>
2017-05-21 12:43:38 +0200

    Add tokenizer

--- src/sabri/syntax/mod.rs
+++ src/sabri/syntax/mod.rs
@@ -1,5 +1,8 @@
-mod token;
-mod block_tree;
+pub mod token;
+pub mod block_tree;
+pub mod tokenizer;
+
 
 pub use token::{Token, TokenType, TokenPosition};
+pub use self::tokenizer::Tokenizer;
 pub use block_tree::{BlockTree, Chunk, ChunkValue};
